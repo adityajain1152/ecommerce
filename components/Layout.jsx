a2deec788162cf498a06d963fe8ac3dd95838631
adityajain1152 <adityajain1152@gmail.com>
2022-11-19 15:51:16 +0530

    completed before stripe

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import  Footer  from './Footer';
 
-export const Layout = () => {
+
+export const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title> Bakchod Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+      {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+
+    </div>
   )
 }
+
+export default Layout
